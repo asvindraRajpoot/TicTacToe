b63adb66b8f57f97a35d2ced692e2c0ea732340d
asvindra <aswin1399904@gmail.com>
2021-12-03 00:38:39 +0530

    Complteted TicTacToe game

---
+++ game/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Components/Header";
+import Game from "./Components/Game";
+import "./Stylesheet/style.css";
+
+function App() {
+  return (
+    <>
+       <Header/>
+       <Game/>
+      
+    </>
+
+  );
+}
+
+export default App;
---
+++ game/src/Components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <>
+           <h1>TicTocToe</h1>
+        </>
+    )
+}
+
+export default Header;
